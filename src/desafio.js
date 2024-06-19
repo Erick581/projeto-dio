843033a9323584a46196ce7aab46a3c29b23aef0
Erick581 <erickgabrielmartines@gmail.com>
2024-06-19 13:00:36 -0300

    meu segundo commit

---
+++ src/desafio.js
@@ -0,0 +1,39 @@
+//variaveis
+
+let nome = "Dhanvad"
+let xpDoHeroi = 7643
+
+console.log(" ")
+console.log("nome do heroi: " + nome)
+console.log(" ")
+
+//estrutura de decisão
+
+if(xpDoHeroi <= 1000){
+    let ferro = "FERRO"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + ferro + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 2000){
+    let bronze = "BRONZE"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + bronze + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 5000){
+    let prata = "PRATA"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + prata + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 7000){
+    let ouro = "OURO"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + ouro + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 8000){
+    let platina = "PLATINA"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + platina + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 9000){
+    let acendente = "ACENDENTE"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + acendente + " com: " + xpDoHeroi + " de xp")
+}else if(xpDoHeroi <= 10000){
+    let imortal = "IMORTAL"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + imortal + " com: " + xpDoHeroi + " de xp")
+}else if (xpDoHeroi > 10000){
+    let radiante = "RADIANTE"
+    console.log("o heroi de nome: " + nome + " esta no rank: " + radiante + " com: " + xpDoHeroi + " de xp")
+}
+
+console.log(" ")
+
